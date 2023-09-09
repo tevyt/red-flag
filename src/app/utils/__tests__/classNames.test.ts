d7acb7fc49e0ed749fce5b8c47abd20da6adf1f6
Travis Smith <travis.a.smith.ja@gmail.com>
2023-09-09 12:12:17 -0400

    chore: Fixes intellisense for Jest and VSCode. (#2)
    VSCode is using the Chai assertions imported with Cypress by
    default. This is leading to issues with intellisense in Jest tests.
    This change explicitly import jest globals in jest tests.

--- src/app/utils/__tests__/classNames.test.ts
+++ src/app/utils/__tests__/classNames.test.ts
@@ -1,3 +1,10 @@
+/**
+ * Cypress uses Chai for assertions, which conflicts with Jest.
+ * VSCode is using the types from Cypress, which is why it's complaining.
+ * This is a workaround to get VSCode to stop complaining.
+ */
+import { describe, it, expect } from "@jest/globals";
+
 import classNames from "../classNames";
 
 describe("classNames", () => {
