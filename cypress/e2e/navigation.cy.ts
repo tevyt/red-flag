b183618e1434676b92dfce72eb78262933e592d8
Travis Smith <travis.a.smith.ja@gmail.com>
2023-09-09 00:18:45 -0400

    test: Adds cypress tests.

---
+++ cypress/e2e/navigation.cy.ts
@@ -0,0 +1,49 @@
+describe("Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("should redirect to the projects page by default", () => {
+    cy.url().should("include", "/projects");
+  });
+
+  describe("desktop", () => {
+    beforeEach(() => {
+      cy.viewport(1280, 720);
+    });
+    it("should navigate to the projects page", () => {
+      cy.get("a[href='/projects']").click();
+      cy.url().should("include", "/projects");
+    });
+    it("should navigate to the users page", () => {
+      cy.get("a[href='/users']").click();
+      cy.url().should("include", "/users");
+    });
+    it("contain a link to the source on Github", () => {
+      cy.get("a[href='https://github.com/tevyt/red-flag']").should("exist");
+    });
+  });
+
+  describe("mobile", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-6");
+    });
+    it("should open the sidebar", () => {
+      cy.get("button[id='sidebar-open']").click();
+      cy.get("div[id='sidebar']").should("exist");
+      cy.get("button[id='sidebar-close']").click();
+      cy.get("div[id='sidebar']").should("not.exist");
+    });
+
+    it("should navigate to the projects page", () => {
+      cy.get("button[id='sidebar-open']").click();
+      cy.get("a[href='/projects']").last().click();
+      cy.url().should("include", "/projects");
+    });
+    it("should navigate to the users page", () => {
+      cy.get("button[id='sidebar-open']").click();
+      cy.get("a[href='/users']").last().click();
+      cy.url().should("include", "/users");
+    });
+  });
+});
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/projects",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
